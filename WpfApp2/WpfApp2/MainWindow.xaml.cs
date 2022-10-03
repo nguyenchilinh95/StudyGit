c4ce815099ded0b343939bbb464277263fe9e90c
Linh_BIMTechSoft <bimnguyenchilinh@gmail.com>
2022-10-03 23:14:29 +0700

    s

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace WpfApp2
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
-            MessageBox.Show("Git3");//////sssssssssssssss\
+            MessageBox.Show("Git6");//////sssssssssssssss\sdasdasdasdasdgit
 
         }
     }
