4c1eac91a6ca5db008af42d6cac7b6a5d7b9e75e
Mr Linh <Mr Linh@DESKTOP-6NRRO83>
2022-10-03 22:49:42 +0700

    git3

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WpfApp2
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Git2");
+            MessageBox.Show("Git3");
             //Commit by gitbash
         }
     }
