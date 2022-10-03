c11ac9c314f2bd9da0cbdc4d0a42438e3113f22b
Mr Linh <Mr Linh@DESKTOP-6NRRO83>
2022-10-03 23:15:01 +0700

    ok

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace WpfApp2
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
-            MessageBox.Show("Git3");//////sssssssssssssss\
+            MessageBox.Show("Git4");//////sssssssssssssss\
 
         }
     }
